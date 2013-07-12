b2e0f51ad2bd8e2beebcf37643f690337614958c
Roland Kuhn <rk@rkuhn.info>
2013-07-12 11:42:49 +0200

    update version to 0.6-SNAPSHOT

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object B extends Build {
 
   override lazy val settings = super.settings ++ Seq(
     organization := "com.typesafe.genjavadoc",
-    version := "0.5",
+    version := "0.6-SNAPSHOT",
     scalaVersion := "2.11.0-M5")
 
   lazy val top = Project(
