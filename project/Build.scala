e4faf7d5418927cb5488b2b88e527f4092c8213d
Roland Kuhn <rk@rkuhn.info>
2014-04-16 22:19:36 +0200

    switch to Scala 2.11.0

--- project/Build.scala
+++ project/Build.scala
@@ -17,7 +17,7 @@ object B extends Build {
     organization := "com.typesafe.genjavadoc",
     version := "0.8-SNAPSHOT",
     scalaVersion := "2.10.4",
-    crossScalaVersions := (0 to 4).map(i => s"2.10.$i") ++ Seq("2.11.0-RC4"),
+    crossScalaVersions := (0 to 4).map(i => s"2.10.$i") ++ Seq("2.11.0"),
     scalaTestVersion := "2.1.3",
     resolvers += Resolver.mavenLocal)
 
