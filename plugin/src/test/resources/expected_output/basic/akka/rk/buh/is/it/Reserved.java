a34c234afaef76618e24ea7bfa744a264bc1b131
Jakob Odersky <jakob@odersky.com>
2016-04-25 14:41:32 -0700

    Mangle static forwarders

---
+++ plugin/src/test/resources/expected_output/basic/akka/rk/buh/is/it/Reserved.java
@@ -0,0 +1,4 @@
+package akka.rk.buh.is.it;
+public abstract class Reserved {
+    public   Reserved ()  { throw new RuntimeException(); }
+}
---
+++ plugin/src/test/resources/expected_output/basic/akka/rk/buh/is/it/ReservedImpl$.java
@@ -0,0 +1,8 @@
+package akka.rk.buh.is.it;
+public class ReservedImpl$ extends akka.rk.buh.is.it.Reserved {
+    /**
+     * Static reference to the singleton instance of this Scala object.
+     */
+    public static final ReservedImpl$ MODULE$ = null;
+    public ReservedImpl$ () { throw new RuntimeException(); }
+}
