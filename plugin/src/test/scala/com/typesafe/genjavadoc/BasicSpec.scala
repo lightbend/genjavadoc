2dc1874f2393a99ef97835cffe28de130e89bce5
Seth Tisue <seth@tisue.net>
2018-02-21 18:07:12 -0800

    handle more Scala version numbers correctly
    annoyingly, a Scala built by the PR validator has an artifact
    version of e.g. `2.13.0-pre-c6065cc-SNAPSHOT` but a
    versionNumberString of e.g. 2.13.0-20180218-172225-c6065cc`.
    the old logic was handling this incorrectly.
    
    with this change, we lose the ability to distinguish between
    e.g. 2.13.0-M2 and 2.13.0-M3.  but it's fine, it's rare we actually
    need to change the patch file, and when a new Scala milestone comes
    out, it's fine to drop support for the previous milestone

--- plugin/src/test/scala/com/typesafe/genjavadoc/BasicSpec.scala
+++ plugin/src/test/scala/com/typesafe/genjavadoc/BasicSpec.scala
@@ -20,11 +20,7 @@ class BasicSpec extends WordSpec with Matchers with CompilerSpec {
 
   override def sources = BasicSpec.sources
   override def expectedPath: String = {
-
-    // nightly Scala versions have version numbers like "2.12.5-bin-3792784".
-    // we just want the "2.12.5" part
-    val scalaVersion = scala.util.Properties.versionNumberString.split("-bin-|-pre-").head
-
+    val scalaVersion = scala.util.Properties.versionNumberString.split("-").head
     val patchFile = s"src/test/resources/patches/$scalaVersion.patch"
     if (new java.io.File(patchFile).exists) { // we have a patch to apply to expected output for this scala version
       "rm -rf target/expected_output".! // cleanup from previous runs
