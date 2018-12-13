ebb91bd48b0f7cc5afaa99378d9b4ed7f1dd4c3e
Arnout Engelen <arnout@bzzt.net>
2018-12-13 15:32:44 +0100

    Scala 2.11 fix (hopefully)

--- plugin/src/test/resources/expected_output/basic/akka/actor/dsl/Inbox.java
+++ plugin/src/test/resources/expected_output/basic/akka/actor/dsl/Inbox.java
@@ -2,6 +2,5 @@ package akka.actor.dsl;
 public  interface Inbox {
   public  interface InboxExtension {
     public  int DSLInboxQueueSize ()  ;
-    public  void akka$actor$dsl$Inbox$InboxExtension$_setter_$DSLInboxQueueSize_$eq (int x$1)  ;
   }
 }
